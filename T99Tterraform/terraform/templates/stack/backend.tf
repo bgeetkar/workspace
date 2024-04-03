3ab584cfe655e5222d334a42154c7d1297e0b850
bgeetkar <141703246+bgeetkar@users.noreply.github.com>
2024-04-03 16:44:18 +0530

    Update backend.tf

--- T99Tterraform/terraform/templates/stack/backend.tf
+++ T99Tterraform/terraform/templates/stack/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "paltform-project-terraform-file"
+    bucket = "paltform-code-terraform-file"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
